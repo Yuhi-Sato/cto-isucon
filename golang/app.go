5db9645bb88c8f66f982836c7e6542a39954e070
Yuhi-Sato <yuhi120101@gmail.com>
2024-07-31 12:34:52 +0900

    makePosts呼び出し箇所をjoin

--- golang/app.go
+++ golang/app.go
@@ -453,7 +453,7 @@ func getAccountName(w http.ResponseWriter, r *http.Request) {
 
 	results := []Post{}
 
-	err = db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `user_id` = ? ORDER BY `created_at` DESC", user.ID)
+	err = db.Select(&results, "SELECT p.`id`, p.`user_id`, p.`body`, p.`mime`, p.`created_at`, u.`account_name` AS \"users.account_name\" FROM `posts` AS p STRAIGHT_JOIN `users` AS u ON p.`user_id` = u.`id` WHERE p.`user_id` = ? AND u.`del_flg` = 0 ORDER BY p.`created_at` DESC LIMIT 20", user.ID)
 	if err != nil {
 		log.Print(err)
 		return
@@ -541,7 +541,7 @@ func getPosts(w http.ResponseWriter, r *http.Request) {
 	}
 
 	results := []Post{}
-	err = db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `created_at` <= ? ORDER BY `created_at` DESC", t.Format(ISO8601Format))
+	err = db.Select(&results, "SELECT p.`id`, p.`user_id`, p.`body`, p.`mime`, p.`created_at`, u.`account_name` AS \"users.account_name\" FROM `posts` AS p STRAIGHT_JOIN `users` AS u ON p.`user_id` = u.`id` WHERE p.`created_at` <= ? AND u.`del_flg` = 0 ORDER BY p.`created_at` DESC LIMIT 20", t.Format(ISO8601Format))
 	if err != nil {
 		log.Print(err)
 		return
@@ -577,7 +577,7 @@ func getPostsID(w http.ResponseWriter, r *http.Request) {
 	}
 
 	results := []Post{}
-	err = db.Select(&results, "SELECT * FROM `posts` WHERE `id` = ?", pid)
+	err = db.Select(&results, "SELECT p.`id`, p.`user_id`, p.`body`, p.`mime`, p.`created_at`, u.`account_name` AS \"users.account_name\" FROM `posts` AS p STRAIGHT_JOIN `users` AS u ON p.`user_id` = u.`id` WHERE p.`id` = ? AND u.`del_flg` = 0 LIMIT 1", pid)
 	if err != nil {
 		log.Print(err)
 		return
